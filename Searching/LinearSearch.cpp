df8b2b7f090ddd4ff6c603f0e8635451b9767974
halit-tugra-celik <64689386+halit-tugra-celik@users.noreply.github.com>
2020-05-02 16:16:33 +0300

    LinearSearch.cpp
    Linear Search Algorithm

---
+++ Searching/LinearSearch.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[1000], n, s;
+void LinearSearch();
+
+int main(){
+	//Getting inputs
+	cin >> n >> s;
+	for(int i = 1;i <= n;i++)
+		cin >> arr[i];
+	
+	//Calling function
+	LinearSearch();	 	
+}
+
+void LinearSearch(){
+ 	int result = -1;//Result value is -1 in the beginning
+	
+	for(int i = 1;i <= n;i++)
+		if(arr[i] == s)//if we find s
+			result = i;//change result as index
+
+	if(result == -1){//if result didn't changed
+		cout << "THERE ISN'T " << s << "IN THE ARRAY\n";
+		return; 	
+	}
+	
+	cout << s << " is in index " << result << '\n'; 
+}
