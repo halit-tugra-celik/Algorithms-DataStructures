124f301f7b16fc58a0de4fc0ee0fe6a561490ebc
halit-tugra-celik <64689386+halit-tugra-celik@users.noreply.github.com>
2020-05-03 07:07:11 +0300

    Update BinarySearch.cpp

--- Searching/BinarySearch.cpp
+++ Searching/BinarySearch.cpp
@@ -5,33 +5,28 @@ int arr[1000], n, s;
 void BinarySearch();
 
 int main(){       
-	//Getting inputs
 	cin >> n >> s;
 	for(int i = 1;i <= n;i++)
 		cin >> arr[i];
 
-	//ARRAY MUST SORTED
-	
-	//sorting array
 	sort(arr + 1, arr + 1 + n);
 
-	//Calling function
 	BinarySearch();	 	
 }
 
 void BinarySearch(){
  	int l = 1, r = n;
-	int mid = (l + r + 1) / 2;//middle of array;
+	int mid = (l + r + 1) / 2;
 	while(l <= r){
 		mid = (l + r + 1) / 2;
-		if(arr[mid] == s)//if middle of array equals s
+		if(arr[mid] == s)
 			break;
-	 	if(arr[mid] > s)//if middle of array bigger than s
+	 	if(arr[mid] > s)
 			r = mid - 1;
-		else//if middle of array smaller than s 
+		else 
 			l = mid + 1;
 	}
-	if(l > r)//didn't find s
+	if(l > r)
 		cout << "THERE ISN'T " << s << " IN THE ARRAY\n";
 	else
 		cout << s << " is in index " << mid << '\n'; 
